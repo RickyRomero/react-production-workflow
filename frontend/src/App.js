238fddc73281742af4d63cda058939c113c2dae9
Ricky Romero <ricky.romero@gmail.com>
2020-08-02 16:57:57 -0700

    Update homepage route message from develop branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from React!
+          Hey, it's time to React!
         </p>
         <a
           className="App-link"
